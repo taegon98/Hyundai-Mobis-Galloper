b00b2a774669332175da5da4fa7eccc15c115a61
taegon <teagon1998@gmail.com>
2023-02-13 17:13:30 +0900

    회원가입 테스트

---
+++ src/test/java/com/server/service/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package com.server.service;
+
+import com.server.domain.Member;
+import com.server.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void join() {
+        Member member = new Member();
+        member.register("test1","test","123","01012345678");
+
+        memberRepository.save(member);
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+
+    @Test
+    void findByTokenId() {
+    }
+}
